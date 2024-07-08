8a906a01c40e52bf80fbf86013d2aba867acb4e8
Denis Wojtowicz <denis.wojtowicz@gmail.com>
2024-07-08 10:03:57 +0200

    hexagonal architecture

---
+++ application/src/test/java/fr/dw/application/ApplicationTests.java
@@ -0,0 +1,8 @@
+package fr.dw.application;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+}
---
+++ infrastructure/src/test/java/fr/dw/infrastructure/InfrastructureApplicationTests.java
@@ -0,0 +1,9 @@
+package fr.dw.infrastructure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfrastructureApplicationTests {
+
+}
